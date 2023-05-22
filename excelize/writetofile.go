b083a3e394817539ed24f63093fa1ef62e5e9ca4
ozzy ozbourne <khanosaid726@gmail.com>
2023-05-22 18:45:50 +0530

    refactored the write function

--- excelize/writetofile.go
+++ excelize/writetofile.go
@@ -8,30 +8,35 @@ import (
 	"strconv"
 )
 
-func WriteToFile(xlsxVal *pbout.XlsxValues) {
+func WriteToFile(xlsxVal *pbout.XlsxValues) error {
+
 	log.Printf("File to be created	-> %s\n"+
 		"Sheet to be created	-> %s\n", xlsxVal.FileName, xlsxVal.SheetName)
 	f := excelize.NewFile()
 
-	defer func() {
+	defer func() error {
 		log.Printf("Closing the file")
 		if err := f.Close(); err != nil {
-			log.Fatalf("Error occurred in closing the xlsx file -> %s\n", err.Error())
+			log.Printf("Error occurred in closing the xlsx file -> %s\n", err.Error())
+			return err
 		}
 		log.Printf("Closed successfully\n")
+		return nil
 	}()
 
 	log.Printf("Creating the\n sheet nameed -> %s\n", xlsxVal.SheetName)
 	index, err := f.NewSheet(xlsxVal.SheetName)
 	if err != nil {
-		log.Fatalf("Error occurred in creating sheet named -> %s\nError -> %s\n", xlsxVal.SheetName, err.Error())
+		log.Printf("Error occurred in creating sheet named -> %s\nError -> %s\n", xlsxVal.SheetName, err.Error())
+		return err
 	}
 
 	log.Printf("Created Successfully\n")
 
 	log.Printf("Deleting the default generated sheet\n")
 	if err := f.DeleteSheet("Sheet1"); err != nil {
-		log.Fatalf("Error occurred in delete default sheet names -> sheet1\n")
+		log.Printf("Error occurred in delete default sheet names -> sheet1\n")
+		return err
 	}
 	log.Printf("Deleted sucessfuly\n")
 
@@ -42,31 +47,36 @@ func WriteToFile(xlsxVal *pbout.XlsxValues) {
 	log.Printf("Looping through the repeated files and save to xlsx\n")
 	for _, v := range xlsxVal.Cols {
 
-		log.Printf("Saving in row -> %d\nFirst name -> %s Last name -> %s Age %d\n",
+		log.Printf("\nSaving in row -> %d\nFirst name -> %s\nLast name -> %s\nAge -> %d\n",
 			v.RowNo, v.FirstName, v.LastName, v.Age)
 
 		row := int(v.RowNo)
 		if err := f.SetCellStr(xlsxVal.SheetName, "A"+strconv.Itoa(row), v.FirstName); err != nil {
-			log.Fatalf("Error occurred in writing First name -> %s\n", v)
+			log.Printf("Error occurred in writing First name -> %s\n", v)
+			return err
 		}
 
 		row++
 		if err := f.SetCellStr(xlsxVal.SheetName, "B"+strconv.Itoa(row), v.LastName); err != nil {
-			log.Fatalf("Error occurred in writing Last name -> %s\n", v)
+			log.Printf("Error occurred in writing Last name -> %s\n", v)
+			return err
 		}
 
 		row++
 		if err := f.SetCellInt(xlsxVal.SheetName, "C"+strconv.Itoa(row), int(v.Age)); err != nil {
-			log.Fatalf("Error occurred in writing Age-> %s\n", v)
+			log.Printf("Error occurred in writing Age-> %s\n", v)
+			return err
 		}
 		log.Printf("ROW Saved Successfully\n")
 	}
 
 	path := filepath.Join("savedxlsxfiles", xlsxVal.FileName+".xlsx")
-	log.Printf("Savint file to disk with path -> %s\n", path)
+	log.Printf("Saving file to disk [LOC] -> %s\n", path)
 	if err := f.SaveAs(path); err != nil {
-		log.Fatalf("Error in saving to disk with file name %s\n", xlsxVal.FileName)
+		log.Printf("Error in saving to disk with file name %s\n", xlsxVal.FileName)
+		return err
 	}
 	log.Printf("Saved Successfully\n")
+	return nil
 
 }
